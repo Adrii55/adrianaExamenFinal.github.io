b42b2ff396c41d6b2d8a820b326441d5cb2cb160
Missael Apaza <missaelapaza@gmail.com>
2025-06-16 02:12:39 -0400

    pipipi

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { Iniciodanzas } from '../pages/Iniciodanzas';
-import { Cards } from '../components/Cards';
-import { Bodymain } from '../components/Bodymain';
+import { Iniciodanzas } from '../pages/Iniciodanzas.jsx';
+import { Cards } from '../components/Cards.jsx';
+import { Bodymain } from '../components/Bodymain.jsx';
 
 export function AppRoutes() {
   return (
     <Routes>
       <Route path="/inicio" element={<Iniciodanzas />} />
       <Route path="/cursos" element={<Cards />} />
-      <Route path="/" element={<Main />} />
+      <Route path="/" element={<Bodymain />} />
     </Routes>
   );
 }
