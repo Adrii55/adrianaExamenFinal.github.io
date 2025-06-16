ecb02e43b43520eae4d8aa7562d501babfc3a401
Missael Apaza <missaelapaza@gmail.com>
2025-06-16 02:18:29 -0400

    sda

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { Iniciodanzas } from '@/pages/Iniciodanzas';
-import { Cards } from '@/components/Cards';
-import { Bodymain } from '@/components/Bodymain';
+import { Iniciodanzas } from '..¿/pages/Iniciodanzas';
+import { Cards } from '../components/Cards';
+import { Bodymain } from '../components/Bodymain';
 
 export function AppRoutes() {
   return (
